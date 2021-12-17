f6c8a106ecc5a2eabb75a2c35220403b1051a701
Murkamilov <Panda.PVO969@yandex.ru>
2021-12-18 01:50:41 +0300

    Исправление после ревью

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-let editBtn = document.querySelector('.profile__btn-edit')
-let popUp = document.querySelector('.popup')
-let closeBtn = document.querySelector('.popup__close')
-let saveBtn = document.querySelector('.popup__save')
-let profileName = document.querySelector('.profile__name')
-let profileDescr = document.querySelector('.profile__name-descr')
-let popupTitle = document.querySelector('.form__name')
-let popupDescr = document.querySelector('.form__descr')
+let editBtn = document.querySelector(".profile__btn-edit");
+let popUp = document.querySelector(".popup");
+let closeBtn = document.querySelector(".popup__close");
+let profileName = document.querySelector(".profile__name");
+let form = document.querySelector(".popup__container");
+let profileDescr = document.querySelector(".profile__name-descr");
+let popupTitle = document.querySelector(".popup__input_name");
+let popupDescr = document.querySelector(".popup__input_profession");
 
 function openPopup() {
-  popUp.classList.add('popup__open');
+  popUp.classList.add("popup__open");
   popupTitle.value = profileName.textContent.trim();
   popupDescr.value = profileDescr.textContent.trim();
-  console.log(profileName.textContent)
-  console.log(profileDescr.textContent)
 }
 
 function closePopup() {
-  popUp.classList.remove('popup__open');
+  popUp.classList.remove("popup__open");
 }
 
-function popupSubmit (evt) {
+function popupSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = popupTitle.value;
   profileDescr.textContent = popupDescr.value;
   closePopup();
 }
 
-editBtn.addEventListener('click', openPopup);
-closeBtn.addEventListener('click', closePopup);
-saveBtn.addEventListener('click', popupSubmit);
+editBtn.addEventListener("click", openPopup);
+closeBtn.addEventListener("click", closePopup);
+form.addEventListener("submit", popupSubmit);
